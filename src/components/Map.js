ca40826aa532b2791fa3ca183f314b4de19c8d32
Nick Wu <nickwu78@gmail.com>
2017-05-22 23:29:30 +1000

    Implemented iss tracking map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,67 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Gmaps, InfoWindow } from 'react-gmaps';
 import './Map.css'
 
+const params = {v: '3.exp', key: 'AIzaSyDX8-TRWtqBRRAegtMRVrbfR7-u_bLnP9o'};
+
 class Map extends Component {
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+    };
+  }
+
+  componentWillMount() {
+    this.getISSPosition();
+  }
+
+  getISSPosition() {
+    axios.get(`http://api.open-notify.org/iss-now.json`)
+      .then(result => {
+        this.setState({
+          latitude: result.data.iss_position.latitude,
+          longitude: result.data.iss_position.longitude
+        })
+      })
+  }
+
+  onMapCreated(map) {
+    map.setOptions({
+      disableDefaultUI: true
+    });
+  }
+
+  onDragEnd(e) {
+    console.log('onDragEnd', e);
+  }
+
+  onClick(e) {
+    console.log('onClick', e);
+  }
+
+  render() {
+    console.log(this.state)
     return (
-      <div>Map Goes Here</div>
+      <div className="map">
+        <Gmaps
+          width={'540px'}
+          height={'300px'}
+          lat={this.state.latitude}
+          lng={this.state.longitude}
+          zoom={3}
+          loadingMessage={'Be happy'}
+          params={params}
+          onMapCreated={this.onMapCreated}>)
+          <InfoWindow
+            lat={this.state.latitude}
+            lng={this.state.longitude}
+            content={'ISS'}
+            onCloseClick={this.onCloseClick} />
+        </Gmaps>
+      </div>
     );
   }
 }
