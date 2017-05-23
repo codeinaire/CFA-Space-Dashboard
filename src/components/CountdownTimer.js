44d867a64ef9d07803a0708e89748274e04833b4
Richard Saville <richardasaville@gmail.com>
2017-05-23 14:49:03 +1000

    call intial API in HumansInSpace

---
+++ src/components/CountdownTimer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import Countdown from 'react-count-down'
+import ReactDOM from 'react-dom'
+
+
+// const TestComponent = () => (
+//   <Countdown options={OPTIONS} />
+// )
+
+const OPTIONS = { endDate: '05/24/2017 01:00 AM', prefix: 'until LIFTOFF!', cb}
+
+const cb = () => {
+  console.log('expired callback')
+}
+
+class CountdownTimer extends Component {
+  render() {
+    return  (
+      <div>
+          <Countdown options={OPTIONS}/>
+      </div>
+    )
+  }
+}
+
+export default CountdownTimer;
